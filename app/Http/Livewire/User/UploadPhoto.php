846693ce8c55874ae93817dbd0b5f3ef2834b5c0
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-12-08 16:47:10 -0500

    Upload de arquivos pt-2

--- app/Http/Livewire/User/UploadPhoto.php
+++ app/Http/Livewire/User/UploadPhoto.php
@@ -3,7 +3,9 @@
 namespace App\Http\Livewire\User;
 
 use Livewire\Component;
+use Illuminate\Support\Str;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Auth;
 
 class UploadPhoto extends Component
 {
@@ -21,6 +23,18 @@ class UploadPhoto extends Component
         $this->validate([
             'photo' => 'required|image|max:1024' //Tamanho maximo 1MB
         ]);
-        dd($this->photo);
+
+        // $this->photo->store('users'); //Usa o mesmo nome do arquivo
+        $user = Auth::user();
+        $nameFile = Str::slug($user->name) . '.' . $this->photo->getClientOriginalExtension();
+        $path = $this->photo->storeAs('users', $nameFile); //Renomeia o arquivo, se houver arquivo com o mesmo nome o arquivo antigo é substituido
+
+        if($path) {
+            $user->update([
+                'profile_photo_path' => $path
+            ]);
+        }
+
+        return redirect()->route('tweets.index');
     }
 }
