a74a286fde00e702e8a4df1c8492bdeae847fb3b
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-12-08 12:34:16 -0500

    Criar componente e data-bind

---
+++ resources/views/livewire/show-tweets.blade.php
@@ -0,0 +1,7 @@
+<div>
+    Show tweets
+
+    <p>{{$message}}</p>
+
+    <input type="text" name="message" id="message" wire:model='message'>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\{
+    ShowTweets
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/tweets', ShowTweets::class);
